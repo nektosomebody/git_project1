92d96069cadbb0fe169dbd2f011ff4759ed85139
ulyana <shirikovaulyanochka@yandex.ru>
2023-01-06 11:10:08 +0300

    add my_superfunction

--- main.py
+++ main.py
@@ -1,11 +1,14 @@
+def my_superfunction():
+    print('What an awesome print!')
+
+
 def main():
     print('My first git program')
     print('And I change it every day')
-    print('And change again')
-    print('Edited')
+    print('Again')
+    print('UFO came and added this line')
+    my_superfunction()
 
 
 if __name__ == '__main__':
     main()
-    print('ab')
-    print('cd')
